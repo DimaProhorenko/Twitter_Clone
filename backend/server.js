2b2eac6088dc156258ea0b258a77c1193dcee092
DimaProkhorenko <dima.study.web@gmail.com>
2024-12-03 13:15:14 +0000

    MongoDB connection, basic server

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import { connectDB } from "./db/connectDB.js";
+
+const PORT = process.env.PORT || 8000;
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connection.once("open", () => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});

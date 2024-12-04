af9ecfee41343ba98a06cb44523de29991b04287
DimaProkhorenko <dima.study.web@gmail.com>
2024-12-04 07:23:45 +0000

    ADD follow unfollow user functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -13,8 +13,56 @@ export const getUserProfile = async (req, res) => {
     return res.status(200).json({ success: true, user });
   } catch (error) {
     console.log(error);
-    return res
-      .status(500)
-      .json({ success: false, msg: "Internal server error" });
+    return res.status(500).json({ success: false, msg: error.message });
+  }
+};
+
+export const followUnfollowUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (id === req.user._id) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "You can't follow yourself" });
+    }
+    const currentUser = await User.findById(req.user._id);
+    const userToFollow = await User.findById(id);
+
+    if (!currentUser || !userToFollow) {
+      return res.status(404).json({ success: false, msg: "User not found" });
+    }
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      // Unfollow logic
+      currentUser.following = currentUser.following.filter(
+        (followId) => followId.toString() !== id
+      );
+      userToFollow.followers = userToFollow.followers.filter(
+        (followerId) => followerId.toString() !== req.user._id.toString()
+      );
+
+      await currentUser.save();
+      await userToFollow.save();
+
+      return res
+        .status(200)
+        .json({ success: true, msg: "Successfully unfollowed the user" });
+    } else {
+      currentUser.following.push(id);
+      userToFollow.followers.push(req.user._id);
+
+      await currentUser.save();
+      await userToFollow.save();
+
+      return res
+        .status(200)
+        .json({ success: true, msg: "Successfully followed the user" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, msg: error.message });
   }
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getUserProfile } from "../controllers/user.controller.js";
+import {
+  followUnfollowUser,
+  getUserProfile,
+} from "../controllers/user.controller.js";
+import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const router = express.Router();
 
-router.get("/profile/:username", getUserProfile);
+router.get("/profile/:username", protectedRoute, getUserProfile);
+router.get("/follow/:id", protectedRoute, followUnfollowUser);
 
 export default router;

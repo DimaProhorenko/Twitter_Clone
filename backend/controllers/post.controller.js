9baa403ea38f3d12ac046314fb2e5e6543c113a1
DimaProkhorenko <dima.study.web@gmail.com>
2024-12-07 21:38:27 +0000

    ADD delete post route

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,8 +1,10 @@
 import Post from "../models/post.model.js";
+import { v2 as cloudinary } from "cloudinary";
 
 export const createPost = async (req, res) => {
   try {
     const { title, text, image } = req.body;
+    let imageUrl = "";
 
     if (!title || !text) {
       return res
@@ -10,10 +12,15 @@ export const createPost = async (req, res) => {
         .json({ success: false, msg: "Title and text fields are required" });
     }
 
-    const post = await new Post({
+    if (image) {
+      const uplodedResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uplodedResponse.secure_url;
+    }
+
+    const post = new Post({
       title,
       text,
-      image,
+      image: imageUrl,
       user: req.user._id,
     });
 
@@ -21,12 +28,46 @@ export const createPost = async (req, res) => {
       return res.status(400).json({ success: false, msg: "Invalid post data" });
     }
 
+    await post.save();
+
     return res.status(200).json({ success: true, post });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
       msg: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const post = await Post.findById(id);
+
+    if (!post) {
+      return res.status(404).json({ success: false, msg: "Post not found" });
+    }
+
+    if (req.user._id.toString() !== post.user._id.toString()) {
+      return res.status(401).json({
+        success: false,
+        msg: "You are not authorized to delete this post",
+      });
+    }
+
+    await post.deleteOne();
+
+    return res
+      .status(200)
+      .json({ success: true, msg: "Post was deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      msg: "Interval server error",
       error: error.message,
     });
   }
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost } from "../controllers/post.controller.js";
 import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const router = express.Router();
 
 router.post("/create", protectedRoute, createPost);
+router.delete("/delete/:id", protectedRoute, deletePost);
 
 export default router;

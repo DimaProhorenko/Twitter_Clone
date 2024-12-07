3899685b3ba84b74f02c58e5820a3903f07dd506
DimaProkhorenko <dima.study.web@gmail.com>
2024-12-07 20:49:27 +0000

    ADD create post route

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,1 +1,33 @@
-export const createPost = async (req, res) => {};
+import Post from "../models/post.model.js";
+
+export const createPost = async (req, res) => {
+  try {
+    const { title, text, image } = req.body;
+
+    if (!title || !text) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Title and text fields are required" });
+    }
+
+    const post = await new Post({
+      title,
+      text,
+      image,
+      user: req.user._id,
+    });
+
+    if (!post) {
+      return res.status(400).json({ success: false, msg: "Invalid post data" });
+    }
+
+    return res.status(200).json({ success: true, post });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      msg: "Internal server error",
+      error: error.message,
+    });
+  }
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { createPost } from "../controllers/post.controller.js";
+import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const router = express.Router();
 
-router.post("/create-post", createPost);
+router.post("/create", protectedRoute, createPost);
 
 export default router;

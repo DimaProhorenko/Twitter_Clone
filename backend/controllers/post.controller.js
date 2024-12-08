6006157ca0189de9d7951a1a94dd0756045d5806
DimaProkhorenko <dima.study.web@gmail.com>
2024-12-08 09:48:37 +0000

    ADD comment on post route

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,5 +1,6 @@
 import Post from "../models/post.model.js";
 import { v2 as cloudinary } from "cloudinary";
+import { getCloudinaryImageId } from "../utils/helpers.js";
 
 export const createPost = async (req, res) => {
   try {
@@ -51,11 +52,15 @@ export const deletePost = async (req, res) => {
       return res.status(404).json({ success: false, msg: "Post not found" });
     }
 
-    if (req.user._id.toString() !== post.user._id.toString()) {
+    if (req.user._id.toString() !== post.user.toString()) {
       return res.status(401).json({
         success: false,
         msg: "You are not authorized to delete this post",
       });
+    }
+
+    if (post.image) {
+      await cloudinary.uploader.destroy(getCloudinaryImageId(post.image));
     }
 
     await post.deleteOne();
@@ -70,5 +75,35 @@ export const deletePost = async (req, res) => {
       msg: "Interval server error",
       error: error.message,
     });
+  }
+};
+
+export const commentOnPost = async (req, res) => {
+  try {
+    const { id: postId } = req.params;
+    const { comment } = req.body;
+    const userId = req.user._id;
+
+    if (!comment) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Comment field is required" });
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ success: false, msg: "Post not found" });
+    }
+
+    post.comments.push({ user: userId, text: comment });
+    await post.save();
+
+    return res.status(200).json({ success: true, msg: "Comment was added" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, msg: "Internal server", error: error.message });
   }
 };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { createPost, deletePost } from "../controllers/post.controller.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+} from "../controllers/post.controller.js";
 import { protectedRoute } from "../middleware/protectedRoute.js";
 
 const router = express.Router();
 
 router.post("/create", protectedRoute, createPost);
 router.delete("/delete/:id", protectedRoute, deletePost);
+router.post("/comment/:id", protectedRoute, commentOnPost);
 
 export default router;

bd59b612bd1870eb61b0bc46803cad471059c293
DimaProkhorenko <dima.study.web@gmail.com>
2024-12-09 20:27:27 +0000

    ADD frontend full

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage, LoginPage, SignupPage } from "./pages";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
